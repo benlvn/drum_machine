ea0bd401361b375cf5f38d58e24646062c7a0f0a
Bennett Levine <ben.lvn@gmail.com>
2017-01-03 19:08:12 -0500

    Initial Commit

---
+++ Beat Buddy Prototype/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Beat Buddy Prototype
+//
+//  Created by Ben Levine on 1/3/17.
+//  Copyright © 2017 Bennett Levine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
