6f9e7dc0cbf284aa5016f2d669197b7ce0f8f6b8
Bennett Levine <ben.lvn@gmail.com>
2017-01-05 13:08:15 -0500

    metronome clicks every beat instead of every sixteenth

--- Beat Buddy Prototype/Model.swift
+++ Beat Buddy Prototype/Model.swift
@@ -7,3 +7,149 @@
 //
 
 import Foundation
+import AVFoundation
+
+class BeatBuddy {
+    
+    private var tempo = 120;
+    private var measure = 0;
+    private var beat = 0;
+    private var sixteenth = 0;
+    private var metronomeOn = false;
+    private var recording = false;
+    private var recordingEnded = false;
+
+    
+    private var fourMeasures = [ [ [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0] ],
+                                 [ [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0] ],
+                                 [ [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0] ],
+                                 [ [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0] ] ]
+    
+    func getTempo() -> Int {
+        return tempo;
+    }
+    
+    func setTempo(newTempo: Int) {
+        tempo = newTempo
+    }
+    
+    
+    
+    func getMeasure() -> Int {
+        return measure
+    }
+    
+    
+    func incrementMeasure() {
+        measure += 1
+        if(measure > 3){
+            measure = 0
+            endRecording()
+        }
+    }
+    
+    func getBeat() -> Int {
+        return beat
+    }
+    
+    var player: AVPlayer! = AVPlayer(url: Bundle.main.url(forResource: "click", withExtension: "mp3")!)
+    
+    func incrementBeat() {
+        player.play()
+        player.seek(to: kCMTimeZero)
+        beat += 1
+        if(beat > 3){
+            if recording {
+                incrementMeasure()
+            }
+            beat = 0
+        }
+    }
+    
+    func getSixteenth() -> Int {
+        return sixteenth
+    }
+    
+    func incrementSixteenth() {
+        sixteenth += 1
+        if(sixteenth > 3){
+            if recording {
+                incrementBeat()
+            }
+            sixteenth = 0
+        }
+    }
+    
+    func metronomeIsOn() -> Bool {
+        return metronomeOn
+    }
+    
+    func startMetronome() {
+        metronomeOn = true
+        recordingEnded = false
+    }
+    
+    func endMetronome() {
+        metronomeOn = false
+    }
+    
+    func isRecording() -> Bool {
+        return recording
+    }
+    
+    func startRecording(){
+        recording = true
+        fourMeasures = [ [ [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0] ],
+                         [ [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0] ],
+                         [ [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0] ],
+                         [ [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0] ] ]
+    }
+    
+    func endRecording() {
+        metronomeOn = false
+        recording = false
+        recordingEnded = true;
+        printMeasures()
+    }
+    
+    func recordingHasEnded() -> Bool{
+        return recordingEnded
+    }
+    
+    func drumHit() {
+        if recording {
+            fourMeasures[measure][beat][sixteenth] = 1
+        }
+        printMeasures()
+    }
+    
+    func printMeasures() {
+        print(getMeasureString(measureNum: 0))
+        print(getMeasureString(measureNum: 1))
+        print(getMeasureString(measureNum: 2))
+        print(getMeasureString(measureNum: 3))
+    }
+    
+    func getMeasureString(measureNum: Int) -> String{
+        let currentMeasure = fourMeasures[measureNum]
+        
+        var isFirstBeat = true
+        
+        var stringToReturn = "[ "
+        for beat in currentMeasure {
+            if isFirstBeat {
+                isFirstBeat = false
+            } else {
+                stringToReturn += "| "
+            }
+            for sixteenth in beat {
+                stringToReturn += "\(sixteenth) "
+            }
+            
+        }
+        
+        stringToReturn += "]"
+        
+        return stringToReturn
+    }
+}
--- Beat Buddy Prototype/ViewController.swift
+++ Beat Buddy Prototype/ViewController.swift
@@ -7,19 +7,102 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var measure1: UILabel!
+    @IBOutlet weak var measure2: UILabel!
+    @IBOutlet weak var measure3: UILabel!
+    @IBOutlet weak var measure4: UILabel!
+    @IBOutlet weak var tempoSlider: UISlider!
+    @IBOutlet weak var tempoLabel: UILabel!
+    
+    private var model = BeatBuddy()
+    private var sixteenthTimer: Timer?
+    private var sixteenthTime: Double {
+        get {
+            return 1/Double(tempo)*60/4
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    
+    var tempo: Int {
+        get {
+            return model.getTempo()
+        }
+        set {
+            tempoLabel.text = "\(newValue) bpm"
+            
+            model.setTempo(newTempo: newValue)
+            
+            if( newValue < 60){
+                self.tempo = 60
+            }
+            
+            if(newValue > 240){
+                self.tempo = 240
+            }
+            
+            tempoSlider.setValue(Float(newValue), animated: false)
+            
+            if model.metronomeIsOn() {
+                new()
+            }
+        }
     }
+    
+    @IBAction func drumHit(_ sender: UIButton) {
+        if(!model.isRecording() && model.metronomeIsOn()){
+            model.startRecording()
+        }
+        if(model.isRecording()){
+            model.drumHit()
+        }
 
+    }
+    
+    
+    @IBAction func incrementTempo(_ sender: UIButton) {
+        tempo += 1
+    }
+    
+    
+    @IBAction func decrementTempo(_ sender: UIButton) {
+        tempo -= 1
+    }
+    
+    func fireSixteenthTimer(timer: Timer){
+        model.incrementSixteenth()
+        if(model.recordingHasEnded()){
+            timer.invalidate()
+            measure1.text = model.getMeasureString(measureNum: 0)
+            measure2.text = model.getMeasureString(measureNum: 0)
+            measure3.text = model.getMeasureString(measureNum: 0)
+            measure4.text = model.getMeasureString(measureNum: 0)
+        } else {
+            if(model.getSixteenth() == 0){
 
+            }
+        }
+    }
+    
+    
+    
+    @IBAction func new() {
+        sixteenthTimer?.invalidate()
+        model.startMetronome()
+        sixteenthTimer = Timer.scheduledTimer(withTimeInterval: sixteenthTime, repeats: true, block: fireSixteenthTimer)
+        
+    }
+    
+    
+    @IBAction func sliderMoved(_ sender: UISlider) {
+        tempo = Int(sender.value)
+
+    }
+    
+    
 }
 
